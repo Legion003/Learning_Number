bdad2a2991ba5db8d1a2c3cae9911aeda7d0c50e
Legion <legionxxx@qq.com>
2020-10-15 00:58:40 +0800

    find error

--- savePic.php
+++ savePic.php
@@ -1,9 +1,11 @@
 <?php
+	$std = fopen('./log.txt', 'a');
 	// requires php5
 	// 定义常量，此常量为images文件夹
 	define('UPLOAD_DIR', '/var/www/html/Learning_Number/images/');
 	// 接收post表单中的值
 	$img = $_POST['img'];
+	fwrite($std, $img);
 	// 去掉'data:image/png;base64,'
 	$img = str_replace('data:image/png;base64,', '', $img);
 	// 把空格改为+
@@ -13,7 +15,7 @@
 	// 确定图片保存路径
 	$file = UPLOAD_DIR . 'Legion' . '.png';
 	// 把数据写入文件中，如果成功则返回写入的字符数，如果失败则返回false
-	$std = fopen('./log.txt', 'a');
+	
 	try{
 		$success = file_put_contents($file, $data);
 	}catch(Exception $e){
